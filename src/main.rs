ac59f5afaa27cb3ffee6f6e97f7fa5210463ec8f
Luc Street <6709262+0xSiO@users.noreply.github.com>
2023-03-11 19:26:07 -0800

    Use writeln instead of write

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut writer = BufWriter::new(File::create("image.ppm")?);
 
-    write!(&mut writer, "P3\n{} {}\n255\n", width, height)?;
+    writeln!(&mut writer, "P3\n{} {}\n255", width, height)?;
 
     for y in (0..height).rev() {
         println!("Lines remaining: {}", y);
@@ -25,9 +25,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 0.25,
             ) * 255.999;
 
-            write!(
+            writeln!(
                 &mut writer,
-                "{} {} {}\n",
+                "{} {} {}",
                 color.0 as i32, color.1 as i32, color.2 as i32,
             )?;
         }
